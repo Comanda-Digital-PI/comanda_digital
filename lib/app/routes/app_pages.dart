2d6cad38dd4a0a3d0377b965a63f7c8bb7f9a52a
gusfrancoo <gustafranco2543@gmail.com>
2025-04-06 15:42:53 -0300

    :broom: cleanup: limpando codigo inutilizado

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_faculdade/app/controllers/navigation_controller.dart';
 import 'package:flutter_faculdade/app/routes/app_routes.dart';
 import 'package:flutter_faculdade/app/screens/configs/config_screen.dart';
-import 'package:flutter_faculdade/app/screens/home_screen.dart';
+import 'package:flutter_faculdade/app/screens/home/home_screen.dart';
 import 'package:flutter_faculdade/app/screens/auth/login_screen.dart';
 import 'package:flutter_faculdade/app/screens/mesas_screen.dart';
 import 'package:flutter_faculdade/app/screens/pedidos_screen.dart';
